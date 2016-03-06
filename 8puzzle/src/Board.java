8077ae15ed1aa6a01047805b821e2972bdab2ff1
Jacky <Jacky@192.168.11.3>
2016-03-06 23:49:12 +0900

    Very minor additions

--- 8puzzle/src/Board.java
+++ 8puzzle/src/Board.java
@@ -11,24 +11,42 @@
 public class Board {
     private int N;
     private int[][] blocks;
-    
+    private int moves;
     
 // construct a board from an N-by-N array of blocks
 // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
-        this.blocks = blocks;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                this.blocks[i][j] = blocks[i][j];
+            }
+        }
     }
 
     public int dimension() {                 // board dimension N
-        return 0;
+        return N*N;
     }
 
     public int hamming() {                   // number of blocks out of place
-        return 0;
+        int count = moves;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if (this.blocks[i][j] == 0) continue;
+                if (this.blocks[i][j] != i*N+(j+1)) count++;
+            }
+        }
+        return count;
     }
 
     public int manhattan() {                // sum of Manhattan distances between blocks and goal
-        return 0;
+        int count = moves;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if (this.blocks[i][j] == 0) continue;
+                if (this.blocks[i][j] != i*N+(j+1)) count += Math.abs(i*N+(j+1)-this.blocks[i][j]);
+            }
+        }
+        return count;
     }
 
     public boolean isGoal() {                // is this board the goal board?
@@ -42,11 +60,27 @@ public class Board {
     }
 
     public Board twin() {                   // a board that is obtained by exchanging any pair of blocks
+        int temp1 = this.blocks[0][0];
+        int temp2 = this.blocks[0][1];
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                this.blocks[i][j] = blocks[i][j];
+            }
+        }
         return this;
     }
 
     public boolean equals(Object y) {       // does this board equal y?
-        return false;
+        if (y == this) return true;
+        if (y == null) return false;
+        if (y.getClass() != this.getClass()) return false;
+        Board that = (Board) y;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(this.blocks[i][j] != that.blocks[i][j]) return false;
+            }
+        }
+        return true;
     }
 
     public Iterable<Board> neighbors() {    // all neighboring boards
@@ -54,13 +88,15 @@ public class Board {
     }
 
     public String toString() {              // string representation of this board (in the output format specified below)
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                if(this.blocks[i][j] != i*N+(j+1) && i != N-1 && j != N-1)
-                    return "none";
+        StringBuilder s = new StringBuilder();
+        s.append(N + "\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                s.append(String.format("%2d ", tiles[i][j]));
             }
+            s.append("\n");
         }
-        return "none";
+        return s.toString();
     }
 
     public static void main(String[] args) {// unit tests (not graded)
